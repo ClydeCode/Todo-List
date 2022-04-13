09b7d26faa45285eb678d4e06402247690ca74cb
Clyde <noneclyde@gmail.com>
2022-04-13 23:04:18 +0300

    Add event listener to buttons

--- src/index.js
+++ src/index.js
@@ -4,3 +4,20 @@ import icon2 from './assets/icon-travel-book.png';
 import icon3 from './assets/icon-traveler.png';
 import icon4 from './assets/layers.png';
 import icon5 from './assets/icon-expand-arrow.png';
+import { initInbox } from './inbox';
+import { initToday } from './today';
+
+const pages = document.querySelectorAll('.page');
+
+initInbox();
+
+pages.forEach(page => page.addEventListener('click', () => {
+    switch(page.id) {
+        case 'inbox':
+            initInbox();
+            break;
+        case 'today':
+            initToday();
+            break;
+    };
+}));
