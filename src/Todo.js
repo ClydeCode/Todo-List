463855617faea6b768f77d4318ba1b80a5a91f7f
Clyde <noneclyde@gmail.com>
2022-04-22 16:42:59 +0300

    Add sort function

--- src/Todo.js
+++ src/Todo.js
@@ -3,6 +3,7 @@ import { getItems } from './storage'
 
 function inboxContent() {
     const arr = getItems();
+    arr.sort((a, b) => new Date (a.date) - new Date (b.date))
     arr.forEach(item => createList(item.title, item.date));
 }
 
