9fe7dd4b2af46d0191849d40c2839ed46594b6f4
Clyde <noneclyde@gmail.com>
2022-04-22 17:18:37 +0300

    Rework code

--- src/Todo.js
+++ src/Todo.js
@@ -1,14 +1,16 @@
 import { createList } from './DOM';
-import { getItems } from './storage'
+import { getItems } from './storage';
+import { compareAsc } from 'date-fns';
+
+const array = getItems();
 
 function inboxContent() {
-    let arr = getItems();
-    arr = sortArray(arr);
+    let arr = sortArray(array);
     arr.forEach(item => createList(item.title, item.date));
 }
 
 function sortArray(array) {
-    return array.sort((a, b) => new Date (a.date) - new Date (b.date));
+    return array.sort((a, b) => compareAsc(new Date(a.date), new Date(b.date)));
 }
 
 export { inboxContent }
