6243541a0d89e2bc5be1e1e8ccd82ced143acf75
Clyde <noneclyde@gmail.com>
2022-04-22 17:46:19 +0300

    Rework code

--- src/pages.js
+++ src/pages.js
@@ -2,29 +2,29 @@ import { setTitle, setActive, rotateArrow, toggleList, wipeContent } from './DOM
 import { inboxContent, todayContent, upcomingContent } from './Todo'
 
 function initInbox() {
-    wipeContent();
-    setActive('inbox');
-    setTitle('Inbox');
+    initialize('inbox');
     inboxContent();
 }
 
 function initToday() {
-    wipeContent();
-    setActive('today');
-    setTitle('Today');
+    initialize('today');
     todayContent();
 }
 
 function initUpcoming() {
-    wipeContent();
-    setActive('upcoming');
-    setTitle('Upcoming');
+    initialize('upcoming');
     upcomingContent();
 }
 
 function expandProjects() {
     rotateArrow();
     toggleList();
+}
+
+function initialize(name) {
+    wipeContent();
+    setActive(name);
+    setTitle(name);
 }
 
 export { initInbox, initToday, initUpcoming, expandProjects }
