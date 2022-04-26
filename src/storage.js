0d423c3e157d6cd2336292deeacd818a86e645d7
Clyde <noneclyde@gmail.com>
2022-04-26 14:35:05 +0300

    Rename an array

--- src/storage.js
+++ src/storage.js
@@ -1,27 +1,27 @@
-let array = [];
+let todos = [];
 
-array[0] = {
+todos[0] = {
     id : 0,
     title : 'pav',
     date : '2022/01/03',
     priority: 0
 }
 
-array[1] = {
+todos[1] = {
     id : 1,
     title : 'pavss',
     date : '2022/04/22',
     priority: 0
 }
 
-array[2] = {
+todos[2] = {
     id : 2,
     title : 'pavssss',
     date : '2022/04/23',
     priority: 0
 }
 
-array[3] = {
+todos[3] = {
     id : 3,
     title : 'pavsssss',
     date : '2022/04/24',
@@ -30,38 +30,38 @@ array[3] = {
 
 function addItem(title, date) {
     const item = {
-        id : array.length,
+        id : todos.length,
         title, 
         date,
         priority : 0
     };
-    array.push(item);
+    todos.push(item);
 }
 
 function removeItem(id) {
-    array.forEach((item, index) => {
+    todos.forEach((item, index) => {
         if (item.id == id) {
-            array.splice(index, 1);
+            todos.splice(index, 1);
         }
     }); 
 }
 
 function setPriority(id, number) {
-    array.forEach(item => {
+    todos.forEach(item => {
         if (item.id == id) item.priority = number;
     });
 }
 
 function showPriority(id) {
     let priority;
-    array.forEach(item => {
+    todos.forEach(item => {
         if (item.id == id) priority = item.priority;
     });
     return priority;
 }
 
 function getItems() {
-    return array;
+    return todos;
 }
 
 export { addItem, removeItem, getItems, setPriority, showPriority }
