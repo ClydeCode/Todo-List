a231c2fd79b2ca4831e5b1f3abd35644c9f62ba5
Clyde <noneclyde@gmail.com>
2022-04-29 17:35:17 +0300

    Clean code

--- src/Todo.js
+++ src/Todo.js
@@ -105,23 +105,4 @@ function changePriority(id) {
     changeIndicatorColor(id);
 }
 
-function detectBrowser(){
-                 
-    let userAgent = navigator.userAgent;
-    
-    if(userAgent.match(/chrome|chromium|crios/i)) {
-        return "chrome";
-      } else if(userAgent.match(/firefox|fxios/i)) {
-        return "firefox";
-      } else if(userAgent.match(/safari/i)) {
-        return "safari";
-      } else if(userAgent.match(/opr\//i)) {
-        return "opera";
-      } else if(userAgent.match(/edg/i)) {
-        return "edge";
-      } else {
-        return "No browser detection";
-      }    
-}
-
 export { inboxContent, todayContent, upcomingContent, changePriority, refresh, deleteTodo, loadProjects, projectContent, deleteProject, editTodoTitle }
