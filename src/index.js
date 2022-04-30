acbed8c118c36ee2758ceb59aea451b7ec26fe61
Clyde <noneclyde@gmail.com>
2022-04-30 16:16:28 +0300

    Add event listener to date picker

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import icon5 from './assets/icon-expand-arrow.png';
 import icon6 from './assets/x.png';
 import { initInbox, initToday, initUpcoming, expandProjects, initProjects, loadProject } from './pages';
 import { changePriority, deleteProject, deleteTodo, editTodoTitle, refresh } from './Todo';
+import { editDate } from './storage';
 
 const pages = document.querySelectorAll('.page');
 
@@ -40,6 +41,13 @@ document.addEventListener('click', (e) => {
             };
         });
     };
+
+    if (e.target.className === 'date-picker') {
+        e.target.addEventListener('change', () => {
+            editDate(e.target.value, e.target.parentNode.children[1].id);
+            refresh();
+        });
+    }
 
     if (e.target.className === 'button-x') {
         deleteTodo(e.target.id);
